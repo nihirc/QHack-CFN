834b04e1843e09533856506f40da4f424b4afa3c
Nihir <nihirc@gmail.com>
2023-02-14 05:40:18 +0000

    Added step 1

--- lambda.py
+++ lambda.py
@@ -1,9 +1,12 @@
 import boto3, json
 import logging
 import cfnresponse
+import os
+import time
 
 logger = logging.getLogger(__name__)
 iam = boto3.client("iam")
+ddb = boto3.client("dynamodb")
 
 def create_iam_user(user):
     response = iam.create_user(
@@ -31,13 +34,37 @@ def create_iam_user_key(user):
     response = iam.create_access_key(
         UserName=user
     )
+    return response
 
+def put_ddb_item(item):
+    response = ddb.put_item(
+        TableName=os.environ["DDB_TABLE"],
+        Item=item
+    )
+    logger.info(response)
+
+def get_ddb_items():
+    response = ddb.scan(TableName=os.environ["DDB_TABLE"])
+    print(response)
     return response
 
 def handler(event, context):
     logger.info(json.dumps(event))
 
     if event['RequestType'] == 'Create':
+        users = event['ResourceProperties']['Users']
+        users = users.split(",")
+        for user in users:
+            if create_iam_user(user):
+                if create_iam_user_profile(user):
+                    response = ddb.add_user_to_group(GroupName=os.environ["GROUP_NAME"], UserName=user)
+                    print(response)
+                    response = create_iam_user_key(user)
+                    item = {}
+                    item["Username"] = user
+                    item["access_key"] = response["AccessKey"]["AccessKeyId"]
+                    item["secret_key"] = response["AccessKey"]["SecretAccessKey"]
+                    put_ddb_item(item)
         response = 'SUCCESS'
         responseData = {}
         responseData['Data'] = response
@@ -50,6 +77,7 @@ def handler(event, context):
         cfnresponse.send(event, context, cfnresponse.SUCCESS, responseData, "CustomResourcePhysicalID")
 
     if event['RequestType'] == 'Delete':
+        response = get_ddb_items()
         response = 'SUCCESS'
         responseData = {}
         responseData['Data'] = response
